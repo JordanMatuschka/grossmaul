7d85587f3a72e9d791bcd9a4dca0bf9925fdb83f
Jordan Matuschka <jordan.matuschka@gmail.com>
2019-08-13 19:39:53 -0400

    indexnt

--- simplemessage.py
+++ simplemessage.py
@@ -5,4 +5,5 @@ from memory import Memory
 memory = Memory()
 
 for line in sys.stdin:
-	memory.addReminder(line.rstrip().lstrip(), time.strftime("%Y-%m-%d")) 
+        memory.addReminder(line.rstrip().lstrip(), time.strftime("%Y-%m-%d"))
+
